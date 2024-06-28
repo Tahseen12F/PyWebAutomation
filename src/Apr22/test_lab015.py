64b8d5472cda364f3d81d5a2c44b8ec98fc25ff6
Tahseen <tahseen123fatma@gmail.com>
2024-06-29 00:12:51 +0530

    Apr22Task

---
+++ src/Apr22/test_lab015.py
@@ -0,0 +1,14 @@
+# Use of CSS Selector
+from selenium import webdriver
+import time
+import pytest
+from selenium.webdriver.common.by import By
+
+
+def test_vwoLogin():
+    driver = webdriver.Chrome()
+    driver.get("https://app.vwo.com")
+    username = driver.find_element(By.CSS_SELECTOR, "#login-username")
+    username.send_keys("a12")
+    time.sleep(5)
+    driver.quit()
